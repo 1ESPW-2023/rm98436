dca9b43d2f9827147987d645df421a57c71afcf7
ruiasiqueira <rm98436@fiap.com.br>
2023-04-11 21:01:35 -0300

    soma

--- js/script.js
+++ js/script.js
@@ -5,8 +5,14 @@ const divElement = document.getElementById("div-box")
 //     console.log(divElement);
 // })
 
-divElement.addEventListener("click", );
+divElement.addEventListener("click", mostraElemento;)
 function mostraElemento() {
     console.log(divElement)
+}
+
+function soma(){
+    let n1 = 10
+    let n2 = 5
+    console.log(n1+n2)
 }
 
