1ddb64b0bc2602035e1d538670f6ac18b930825b
ruiasiqueira <rm98436@fiap.com.br>
2023-04-18 20:20:23 -0300

    Collections- SPREAD-Arrays-Map

--- js/script.js
+++ js/script.js
@@ -1,18 +1,40 @@
 //Exemplo de Funções
-const divElement = document.getElementById("div-box")
+// const divElement = document.getElementById("div-box")
 
-// divElement.addEventListener("click", ()=>{
-//     console.log(divElement);
-// })
+// // divElement.addEventListener("click", ()=>{
+// //     console.log(divElement);
+// // });
 
-divElement.addEventListener("click", mostraElemento;)
-function mostraElemento() {
-    console.log(divElement)
-}
+// divElement.addEventListener("click", mostraElemento);
 
-function soma(){
-    let n1 = 10
-    let n2 = 5
-    console.log(n1+n2)
-}
+// function mostraElemento() {
+//     console.log(divElement)
+// }
 
+// function soma() {
+//     let n1 = 10
+//     let n2 = 5
+//     console.log(n1 + n2)
+// }
+
+//Recuperando Elementos do HTML com querySelector
+// const liElement = document.querySelectorAll("li");
+
+// //Convertendo Node List em Array com Spread
+// const arrayElement = [...liElement];
+
+
+// console.log(liElement);
+
+
+// const liElement = document.querySelectorAll("li");
+
+//Convertendo Node List em Array com Spread
+const arrayElement = [...document.querySelectorAll("li")];
+
+arrayElement.map((el,key)=>{
+    if(el.innerText.indexOf("1",0) {
+        el.textContent = `[ ${el.textContent}"Item-1"]`;
+    }
+
+});
