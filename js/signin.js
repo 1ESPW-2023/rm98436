b27e11fa9b4a808169af58fd879d45d3b332fe2b
ruiasiqueira <rm98436@fiap.com>
2023-05-09 20:05:56 -0300

    login

---
+++ js/signin.js
@@ -0,0 +1,39 @@
+"use strict";
+
+//CRIANDO UM OBJETO
+const usuario1 = {
+    nomeUsuario:"pt001",
+    senhaUsuario:"12345"
+}
+
+const usuario2 = {
+    nomeUsuario:"pt002",
+    senhaUsuario:"12345"
+}
+
+//CRIAR UMA LISTA DE OBJETOS
+let listaDeUsuarios = [];
+listaDeUsuarios.push(usuario1);
+listaDeUsuarios.push(usuario2);
+
+// listaDeUsuarios.forEach( (usuario)=>{
+    
+//     console.log("NOME DE USUÁRIO : " + usuario.nomeUsuario);
+//     console.log("SENHA DE USUÁRIO : " + usuario.senhaUsuario);
+
+// });
+
+
+addEventListener("click", (evt)=>{
+    if(evt.target.id == "btnSubmit"){
+
+        let usuarioInput = document.querySelector("#usuario").value;
+        let senhaInput = document.querySelector("#senha").value;
+
+        if(usuarioInput == "pf0670" && senhaInput == "12345"){
+            console.log("USUÁRIO VALIDADO!");
+        }else{
+            console.log("SENHA OU NOME DE USUÁRIO INVÁLIDO!");
+        }   
+    }
+});
